1b071a871b27f6bbf104a887db8fdf65218874bc
Stan Ng / BuGs <gnubugs@gmail.com>
2023-10-10 17:53:34 +0800

    Wilco project management

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from datetime import date
-from odoo import api, fields, models
-
+from odoo import api, fields, models, _
 
 class HospitalPatient(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
@@ -17,6 +16,13 @@ class HospitalPatient(models.Model):
         ('female', 'Female')
     ], string='Gender', tracking=True, default='female')
     active = fields.Boolean(string='Active', default=True)
+    analytic_account_id = fields.Many2one('account.analytic.account', string="Analytic Account", copy=False,
+                                          ondelete='set null',
+                                          domain="[('company_id', '=', False)]",
+                                          check_company=True)
+
+    def _compute_company_id(self):
+        return self.env.company
 
     @api.depends('date_of_birth')
     def _compute_age(self):
@@ -37,3 +43,46 @@ class HospitalPatient(models.Model):
         return today.year \
                    - patient.date_of_birth.year \
                    - ((today.month, today.day) < (patient.date_of_birth.month, patient.date_of_birth.day))
+
+    def _create_analytic_account(self):
+        company = self.env.company
+        for patient in self:
+            analytic_account = self.env['account.analytic.account'].create({
+                'name': patient.name,
+                'company_id': company.id,
+                'plan_id': company.analytic_plan_id.id,
+                'active': True,
+            })
+            patient.write({'analytic_account_id': analytic_account.id})
+
+    @api.model
+    def _create_analytic_account_from_values(self, values):
+        company = self.env.company
+        name = values.get('name', _('Unknown Analytic Account'))
+        analytic_account = self.env['account.analytic.account'].create({
+            'name': name,
+            'company_id': company.id,
+            'plan_id': company.analytic_plan_id.id,
+            'active': True,
+        })
+        return analytic_account
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """ Create an analytic account if don't provide one
+            Note: create it before calling super() to avoid raising the ValidationError from _check_allow_timesheet
+        """
+        defaults = self.default_get(['analytic_account_id'])
+        for vals in vals_list:
+            analytic_account_id = vals.get('analytic_account_id', defaults.get('analytic_account_id'))
+            if not analytic_account_id:
+                analytic_account = self._create_analytic_account_from_values(vals)
+                vals['analytic_account_id'] = analytic_account.id
+        return super().create(vals_list)
+
+    def write(self, values):
+        if not values.get('analytic_account_id'):
+            for patient in self:
+                if not patient.analytic_account_id:
+                    patient._create_analytic_account()
+        return super(HospitalPatient, self).write(values)
--- custom_addons/wilco_project/models/project.py
+++ custom_addons/wilco_project/models/project.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 
 class Project(models.Model):
     _inherit = 'project.project'
@@ -16,3 +16,54 @@ class Project(models.Model):
                 name = project.name
             result.append((project.id, name))
         return result
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """ Create an analytic account if don't provide one
+            Note: create it before calling super() to avoid raising the ValidationError from _check_allow_timesheet
+        """
+        defaults = self.default_get(['analytic_account_id'])
+        for vals in vals_list:
+            analytic_account_id = vals.get('analytic_account_id', defaults.get('analytic_account_id'))
+            if not analytic_account_id:
+                analytic_account = self._create_analytic_account_from_values(vals)
+                vals['analytic_account_id'] = analytic_account.id
+        return super().create(vals_list)
+
+    def write(self, values):
+        if not values.get('analytic_account_id'):
+            for project in self:
+                if not project.analytic_account_id:
+                    project._create_analytic_account()
+
+        result = super(Project, self).write(values) if values else True
+
+        if 'partner_id' in values and self.analytic_account_id:
+            projects_read_group = self.env['project.project']._read_group(
+                [('analytic_account_id', 'in', self.analytic_account_id.ids)],
+                ['analytic_account_id'],
+                ['analytic_account_id']
+            )
+            analytic_account_to_update = self.env['account.analytic.account'].browse([
+                result['analytic_account_id'][0]
+                for result in projects_read_group
+                if result['analytic_account_id'] and result['analytic_account_id_count'] == 1
+            ])
+            analytic_account_to_update.write({'partner_id': self.partner_id})
+
+        return result
+
+    def wilco_action_view_analytic_account_entries(self):
+        self.ensure_one()
+        return {
+            'res_model': 'account.analytic.line',
+            'type': 'ir.actions.act_window',
+            'name': _("Gross Margin"),
+            'domain': [('account_id', '=', self.analytic_account_id.id)],
+            'views': [(self.env.ref('analytic.view_account_analytic_line_tree').id, 'list'),
+                      (self.env.ref('analytic.view_account_analytic_line_form').id, 'form'),
+                      (self.env.ref('analytic.view_account_analytic_line_graph').id, 'graph'),
+                      (self.env.ref('analytic.view_account_analytic_line_pivot').id, 'pivot')],
+            'view_mode': 'tree,form,graph,pivot',
+            'context': {'search_default_group_date': 1, 'default_account_id': self.analytic_account_id.id}
+        }
