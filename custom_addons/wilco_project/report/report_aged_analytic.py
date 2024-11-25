4a08eab46e4a227bd1bb7fafd8fb1ead20c25f36
Stan Ng / BuGs <gnubugs@gmail.com>
2024-11-25 15:35:50 +0800

    Wilco project management 1. Fix aged analytic account report (Beta)

--- custom_addons/wilco_project/report/report_aged_analytic.py
+++ custom_addons/wilco_project/report/report_aged_analytic.py
@@ -9,19 +9,17 @@ from dateutil.relativedelta import relativedelta
 
 
 class ReportAgedAnalyticBalance(models.AbstractModel):
-    _name = 'report.wilco_project.report_aged_analytic'
+    _name = 'report.wilco_project.report_agedanalyticbalance'
     _description = 'Aged Analytic Balance Report'
 
-    def _get_analytic_move_lines(self, account_type, analytic_account_ids,
-                              date_from, target_move, period_length):
+    def _get_analytic_move_lines(self, account_type, analytic_account_ids, date_from, target_move, period_length):
         periods = {}
-        date_from = datetime.strptime(str(date_from), "%Y-%m-%d").date()
         start = datetime.strptime(str(date_from), "%Y-%m-%d")
-        
-        # Define periods like aged partner balance
+        date_from = datetime.strptime(str(date_from), "%Y-%m-%d").date()
+
         for i in range(5)[::-1]:
             stop = start - relativedelta(days=period_length)
-            period_name = str((5-(i+1)) * period_length) + '-' + str((5-i) * period_length)  # Removed +1
+            period_name = str((5-(i+1)) * period_length) + '-' + str((5-i) * period_length)
             period_stop = (start - relativedelta(days=1)).strftime('%Y-%m-%d')
             if i == 0:
                 period_name = '+' + str(4 * period_length)
@@ -34,34 +32,69 @@ class ReportAgedAnalyticBalance(models.AbstractModel):
 
         # Initialize results
         analytic_lines = {}
-        totals = [0] * 7  # [Not due, 0-30, 30-60, 60-90, 90-120, +120, Total]
+        totals = [0] * 7
 
-        # Query for not due amounts (future dates)
-        query_future = """
-            SELECT aml.analytic_distribution, aml.balance as amount
-            FROM account_move_line aml
-            JOIN account_move am ON am.id = aml.move_id
-            JOIN account_account acc ON acc.id = aml.account_id
-            WHERE am.state IN %s
-                AND acc.account_type IN %s
-                AND (COALESCE(aml.date_maturity,aml.date) >= %s)
-                AND aml.analytic_distribution IS NOT NULL
-                AND aml.date <= %s
-        """
-        
+        # Query for move lines
         move_state = ['posted'] if target_move == 'posted' else ['draft', 'posted']
-        self.env.cr.execute(query_future, (tuple(move_state), tuple(account_type), date_from, date_from))
-        
-        # Rest of your existing code...
+        query = '''
+            SELECT aml.id
+            FROM account_move_line aml
+            JOIN account_move am ON (am.id = aml.move_id)
+            JOIN account_account acc ON (aml.account_id = acc.id)
+            WHERE (aml.date <= %s) AND (am.state IN %s)
+            AND (acc.account_type IN %s)
+            AND aml.analytic_distribution IS NOT NULL
+        '''
+        params = (date_from, tuple(move_state), tuple(account_type))
+        self.env.cr.execute(query, params)
+        aml_ids = [x[0] for x in self.env.cr.fetchall()]
+
+        # Process each move line
+        for line in self.env['account.move.line'].browse(aml_ids):
+            analytic_dist = line.analytic_distribution or {}
+            for analytic_id, percentage in analytic_dist.items():
+                if not analytic_account_ids or int(analytic_id) in analytic_account_ids:
+                    analytic_id = int(analytic_id)
+                    if analytic_id not in analytic_lines:
+                        analytic_name = self.env['account.analytic.account'].browse(analytic_id).name
+                        analytic_lines[analytic_id] = {
+                            'name': analytic_name,
+                            'lines': [0] * 7,
+                            'total': 0.0,
+                        }
+
+                    amount = line.balance * (percentage / 100.0)
+                    date = line.date_maturity or line.date
+                    period = 6
+
+                    if date <= date_from:
+                        for i in range(5):
+                            stop = periods[str(i)]['stop']
+                            start = periods[str(i)]['start']
+                            if start and stop and start <= date.strftime('%Y-%m-%d') <= stop:
+                                period = i
+                                break
+                        if period == 6:
+                            period = 0
+
+                    analytic_lines[analytic_id]['lines'][period] += amount
+                    analytic_lines[analytic_id]['total'] += amount
+                    totals[period] += amount
+
+        # Convert periods dictionary to list for template rendering
+        periods_list = []
+        for i in range(5):
+            periods_list.append(periods[str(i)])
+
+        return list(analytic_lines.values()), totals, periods_list
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        if not data.get('form') or not self.env.context.get('active_model') or not self.env.context.get('active_id'):
+        if not data.get('form'):
             raise UserError(_("Form content is missing, this report cannot be printed."))
 
-        model = self.env.context.get('active_model')
-        docs = self.env[model].browse(self.env.context.get('active_id'))
-
+        data['computed'] = {}
+        obj_partner = self.env['res.partner']
         target_move = data['form'].get('target_move', 'all')
         date_from = data['form'].get('date_from', time.strftime('%Y-%m-%d'))
 
@@ -77,16 +110,16 @@ class ReportAgedAnalyticBalance(models.AbstractModel):
             data['form'].get('analytic_account_ids', []),
             date_from,
             target_move,
-            data['form'].get('period_length', 30)
+            data['form']['period_length']
         )
 
         return {
-            'doc_ids': self.ids,
-            'doc_model': model,
+            'doc_ids': data['form'].get('analytic_account_ids', []),
+            'doc_model': 'account.analytic.account',
             'data': data['form'],
-            'docs': docs,
+            'docs': self.env['account.analytic.account'].browse(data['form'].get('analytic_account_ids', [])),
             'time': time,
             'get_analytic_lines': movelines,
             'get_direction': total,
-            'periods': periods
+            'periods': periods,
         }
