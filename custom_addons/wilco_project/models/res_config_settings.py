972b9ec1728e991e55ed635be0663185a3bdcb11
Stan Ng <gnubugs@gmail.com>
2025-06-25 23:17:21 +0800

    [REM] wilco_project: disable incomplete terms update feature
    The terms update functionality was causing errors and was marked as TODO
    with incomplete implementation. Users were encountering error pages when
    attempting to use this feature from the configuration settings.
    
    This temporarily disables the terms update button and method to prevent
    user confusion and errors until the feature can be properly implemented
    with the web page editor integration.

--- custom_addons/wilco_project/models/res_config_settings.py
+++ custom_addons/wilco_project/models/res_config_settings.py
@@ -22,18 +22,18 @@ class ResConfigSettings(models.TransientModel):
             # to avoid landing on an error page (see terms.py controller)
             setting.wilco_sale_terms_preview_ready = self.env.company.wilco_sale_terms_type == 'html' and setting.wilco_sale_terms_type == 'html'
 
-    #TODO: Terms form for sale term not yet done
-    def wilco_action_update_terms(self):
-        raise UserError('Quotation/Order terms setup by Web Page not yet done')
-        self.ensure_one()
-        if hasattr(self, 'website_id') and self.env.user.has_group('website.group_website_designer'):
-            return self.env["website"].get_client_action('/terms', True)
-        return {
-            'name': _('Update Terms & Conditions (Quotation/Order)'),
-            'type': 'ir.actions.act_window',
-            'view_mode': 'form',
-            'res_model': 'res.company',
-            'view_id': self.env.ref("account.res_company_view_form_terms", False).id,
-            'target': 'new',
-            'res_id': self.company_id.id,
-        }
+    # #TODO: Terms form for sale term not yet done
+    # def wilco_action_update_terms(self):
+    #     raise UserError('Quotation/Order terms setup by Web Page not yet done')
+    #     self.ensure_one()
+    #     if hasattr(self, 'website_id') and self.env.user.has_group('website.group_website_designer'):
+    #         return self.env["website"].get_client_action('/terms', True)
+    #     return {
+    #         'name': _('Update Terms & Conditions (Quotation/Order)'),
+    #         'type': 'ir.actions.act_window',
+    #         'view_mode': 'form',
+    #         'res_model': 'res.company',
+    #         'view_id': self.env.ref("account.res_company_view_form_terms", False).id,
+    #         'target': 'new',
+    #         'res_id': self.company_id.id,
+    #     }
